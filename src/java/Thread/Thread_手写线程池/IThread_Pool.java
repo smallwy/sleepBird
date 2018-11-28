1d00e5873733e5856da8798b941134ed007cb920
wangyu <742868088@qq.com>
2018-11-28 16:00:50 +0800

    ：â“fisrt

--- src/java/Thread/Thread_手写线程池/IThread_Pool.java
+++ src/java/Thread/Thread_手写线程池/IThread_Pool.java
@@ -3,7 +3,7 @@ package Thread.Thread_手写线程池;
 import java.util.List;
 
 /**
- * 手写线程池
+ * 手写线程池git
  *
  * @author wy
  * @create 2018-11-28 14:24
@@ -13,13 +13,15 @@ public interface IThread_Pool {
   /** 加入任务 */
   void execute(Runnable task);
 
-  /** 加入任务 */
+
+    /**
+     * 加入任务
+     */
   void execute(Runnable[] tasks);
 
-  /**
-   * 任务lie00
-   * 列表
-   */
+    /**
+     * 任务lie00 列表
+     */
   void execute(List<Runnable> tasks);
 
   /** 销毁线程 */
