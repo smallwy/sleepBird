90631e9f3e0c2b627afc2a93d6540b5c6fb7adf8
smallwy <742868088@qq.com>
2022-06-20 20:15:49 +0800

    mq学习

---
+++ rabbitmq/src/main/java/topic/Publish.java
@@ -0,0 +1,38 @@
+package topic;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import utils.RabbitConstant;
+import utils.RabbitUtils;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+//交换机根据routeKey决定 必须适应routeKey通配规则  发布订阅模式
+public class Publish {
+    public final static Map<String, String> info = new HashMap<>();
+
+    static {
+        info.put("wuhan.2022620", "小雨到中雨");
+        info.put("wuhan.2022621", "小雨到多雲");
+
+        info.put("guangzhou.2022620", "天晴");
+        info.put("guangzhou.2022621", "天晴转暴雨");
+    }
+
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Connection connection = RabbitUtils.getConnection();
+        Channel channel = connection.createChannel();
+        //1 队列名称 2是否持久化 3 是否私有化 4 是否自动删除
+        for (Map.Entry<String, String> entry : info.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            channel.basicPublish(RabbitConstant.EXCHANGE_TOPIC, key, null, value.getBytes());
+        }
+        channel.close();
+        connection.close();
+    }
+}
--- rabbitmq/src/main/java/utils/RabbitConstant.java
+++ rabbitmq/src/main/java/utils/RabbitConstant.java
@@ -12,4 +12,8 @@ public class RabbitConstant {
     public static final String QUEUE_WUHAN = "queueWuhan"; //武汉队列
     public static final String QUEUE_GUANGZHOU = "queueGuangzhou"; //广州队列
 
+    public static final String EXCHANGE_TOPIC = "exchange_routing_topic"; //topic交换机
+    public static final String QUEUE_WUHAN_TOPIC = "queueWuhanTopic"; //武汉队列
+    public static final String QUEUE_GUANGZHOU_TOPIC = "queueGuangzhouTopic"; //广州队列
+
 }
