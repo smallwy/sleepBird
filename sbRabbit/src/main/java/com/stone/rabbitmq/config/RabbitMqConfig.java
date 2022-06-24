a84914ec731e579671f7c3e53ddf613ca33763e3
smallwy <742868088@qq.com>
2022-06-24 10:52:34 +0800

    springboot整合rabbitmq

---
+++ sbRabbit/src/main/java/com/stone/rabbitmq/config/RabbitMqConfig.java
@@ -0,0 +1,14 @@
+package com.stone.rabbitmq.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    //声明交换器
+
+    //声明队列
+
+
+}
