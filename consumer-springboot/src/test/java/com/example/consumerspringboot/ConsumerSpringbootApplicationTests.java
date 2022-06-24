84e1a2363d3cc02bb4712a7d84d1d41a38fb96ab
smallwy <742868088@qq.com>
2022-06-24 15:38:25 +0800

    springboot整合rabbitmq

---
+++ consumer-springboot/src/test/java/com/example/consumerspringboot/ConsumerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consumerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
